1aa8782b656c6a2ef023b464ef0d9703f251dcb4
Giles <gilespupilasset@gmail.com>
2022-07-14 08:41:13 +0100

    More author routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 // All authors route
 router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }
     try {
-        const authors = await Author.find({});
-        res.render('authors/index', { authors: authors });
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        });
     } catch (error) {
         res.redirect('/');
     }
