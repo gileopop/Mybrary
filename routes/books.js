64a265614734e4e23d713583f31abfef8b2fc86d
Giles <gilespupilasset@gmail.com>
2022-07-19 08:10:13 +0100

    Books create/index/new

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const router = express.Router();
-const Author = require('../models/author');
+const fs = require('fs');
 const Book = require('../models/book');
 const uploadPath = path.join('public', Book.coverImageBasePath);
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+const Author = require('../models/author');
 const upload = multer({
     dest: uploadPath,
     fileFilter: (req, file, callback) => {
@@ -15,17 +16,35 @@ const upload = multer({
 
 // All books route
 router.get('/', async (req, res) => {
-    res.send('All books');
+    let query = Book.find();
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'));
+    }
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publicationDate', req.query.publishedBefore);
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publicationDate', req.query.publishedAfter);
+    }
+    try {
+        const books = await query.exec();
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        });
+    } catch {
+        res.redirect('/');
+    }
 });
 
 // New book route
-router.get('/new', (req, res) => {
+router.get('/new', async (req, res) => {
     renderNewPage(res, new Book());
 });
 
 // Create book route 
 router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.filename : null;
+    const fileName = req.file != null ? req.file.filename: null;
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
@@ -38,11 +57,20 @@ router.post('/', upload.single('cover'), async (req, res) => {
     try {
         const newBook = await book.save();
         // res.redirect(`books/${newBook.id}`);
-        res.redirect('books');
+        res.redirect('/books');
     } catch {
+        if (book.coverImageName != null) {
+            removeBookCover(book.coverImageName);
+        }
         renderNewPage(res, book, true);
     }
 });
+
+function removeBookCover(fileName) {
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if (err) console.log(err);
+    });
+}
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -51,10 +79,12 @@ async function renderNewPage(res, book, hasError = false) {
             authors: authors,
             book: book
         };
-        if (hasError) params.errorMessage = 'Error creating book.';
+        if (hasError) {
+            params.errorMessage = 'Error creating book.';
+        }
         res.render('books/new', params);
     } catch {
-        res.redirect('/ books');
+        res.redirect('/books');
     }
 }
 
