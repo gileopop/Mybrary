7c5ce87d79fb45692067e1d7380b5af1af818539
Giles <gilespupilasset@gmail.com>
2022-07-28 16:51:31 +0100

    Finished author routes

--- routes/books.js
+++ routes/books.js
@@ -48,7 +48,6 @@ router.post('/', async (req, res) => {
 
     try {
         const newBook = await book.save();
-        // res.redirect(`books/${newBook.id}`);
         res.redirect('/books');
     } catch {
         renderNewPage(res, book, true);
